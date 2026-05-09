2fe2f6c1b1f9834c8a048c7e3e07412e0c198d4b
Sathyaraj Medipalli <sathyalhr143@gmail.com>
2026-05-09 12:33:42 +0000

    add slam_toolbox and amcl configurations with associated launch files

--- src/bumperbot_localization/launch/global_localization.launch.py
+++ src/bumperbot_localization/launch/global_localization.launch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-
+import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
@@ -19,10 +19,17 @@ def generate_launch_description():
         default_value="small_house"
     )
 
+    amcl_config_arg = DeclareLaunchArgument(
+        "amcl_config",
+        default_value = os.path.join(get_package_share_directory("bumperbot_localization"), "config", "amcl.yaml")
+    )
+
     map_name = LaunchConfiguration("map_name")
     use_sim_time = LaunchConfiguration("use_sim_time")
-
-    lifecycle_nodes = ['map_server']
+    amcl_config = LaunchConfiguration("amcl_config")
+    
+    # Nodes managed my lifecycle_manager
+    lifecycle_nodes = ['map_server', 'amcl']
 
     map_path = PathJoinSubstitution([
         get_package_share_directory("bumperbot_mapping"),
@@ -42,6 +49,16 @@ def generate_launch_description():
         ]
     )
 
+    nav2_amcl = Node(
+        package="nav2_amcl",
+        executable="amcl",
+        name="amcl",
+        output="screen",
+        parameters=[amcl_config,
+            {"use_sim_time": use_sim_time}],
+    
+    )
+
     nav2_lifecycle_manager = Node(
         package="nav2_lifecycle_manager",
         executable="lifecycle_manager",
@@ -57,6 +74,8 @@ def generate_launch_description():
     return LaunchDescription([
         map_name_arg,
         use_sim_time_arg, 
+        amcl_config_arg,
         nav2_map_server,
+        nav2_amcl,
         nav2_lifecycle_manager
     ])
---
+++ src/bumperbot_mapping/launch/slam.launch.py
@@ -0,0 +1,77 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+
+    lifecycle_nodes = ["map_saver_server"]
+
+    ros_distro = os.getenv("ROS_DISTRO")
+
+    if ros_distro != "humble":
+        lifecycle_nodes.append("slam_toolbox")
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="true"
+    )
+
+    slam_config_arg = DeclareLaunchArgument(
+        "slam_config",
+        default_value=os.path.join(
+            get_package_share_directory("bumperbot_mapping"),
+            "config",
+            "slam_toolbox.yaml"
+        )
+    )
+
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    slam_config = LaunchConfiguration("slam_config")
+
+    slam_toolbox_node = Node(
+        package="slam_toolbox",
+        executable="sync_slam_toolbox_node",
+        name="slam_toolbox",
+        output="screen",
+        parameters=[
+            {"use_sim_time": use_sim_time},
+            slam_config,
+        ]
+    )
+
+    nav2_map_saver = Node(
+        package="nav2_map_server",
+        executable="map_saver_server",
+        name="map_saver_server",
+        output="screen",
+        parameters=[
+            {"save_map_timeout": 5.0},
+            {"use_sim_time": use_sim_time},
+            {"free_thresh_default": 0.196},
+            {"occupied_thresh_default": 0.65},
+        ]
+    )
+
+    nav2_lifecycle_manager = Node(
+        package="nav2_lifecycle_manager",
+        executable="lifecycle_manager",
+        name="lifecycle_manager",
+        output="screen",
+        parameters=[
+            {"node_names": lifecycle_nodes},
+            {"use_sim_time": use_sim_time},
+            # {"autostart": True},
+        ]
+    )
+
+    return LaunchDescription([
+        use_sim_time_arg,
+        slam_config_arg,
+        slam_toolbox_node,
+        nav2_map_saver,
+        nav2_lifecycle_manager,
+    ])
